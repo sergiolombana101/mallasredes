b48db8e3aebbb47510fe9ee5759677e28bc6d9b3
sergiolombana <sergiolombana101@gmail.com>
2020-05-28 14:44:14 -0400

    Adding landing page

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.listen(process.env.PORT || 8080);
 app.get('/*', function(req,res){
     res.sendFile(path.join(__dirname+'/dist/index.html'));
 })
-var nodemailer = require('nodemailer');
+/*var nodemailer = require('nodemailer');
 
 function sendEmail(values){
     var transporter = nodemailer.createTransport({
@@ -45,4 +45,4 @@ function sendEmail(values){
             res.status(200)
         }
     });
-}
+}*/
