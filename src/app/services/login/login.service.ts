15a98eb8414c0615b8253d3815be39c42fd7a998
Sergio Lombana <Sergio>
2020-06-24 17:57:16 -0400

    Initial commit

--- src/app/services/login/login.service.ts
+++ src/app/services/login/login.service.ts
@@ -15,7 +15,7 @@ const key = 'redesmallas'
 export class LoginService {
   //port = process.env.PORT || 5000;
   //endPoint = 'https://mallas-server.herokuapp.com/api';
-  endPoint = '/api';
+  endPoint = 'http://127.0.0.1:8080/api';
   response: any;
 
   constructor(private http:Http) { }
