5f4b08627fbd117e834adc4246ef5077872b20ce
Sergio Lombana <Sergio>
2020-06-24 19:49:53 -0400

    Initial commit

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var nodemailer = require('nodemailer');
 
 const app = express();
 const port = process.env.PORT || 8080;
-
+//change
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
