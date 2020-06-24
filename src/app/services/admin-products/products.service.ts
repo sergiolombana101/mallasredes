a7e202c73672f4e0322fc0094e3d120d9e085120
Sergio Lombana <Sergio>
2020-06-24 19:33:37 -0400

    Initial commit

--- src/app/services/admin-products/products.service.ts
+++ src/app/services/admin-products/products.service.ts
@@ -13,7 +13,7 @@ declare const Buffer:any;
 export class ProductsService {
 
   //endPoint = 'https://mallas-server.herokuapp.com/api';
-  endPoint = 'api';
+  endPoint = '/api';
   response: any;
 
   constructor(private http:Http, private httpClient:HttpClient) {
